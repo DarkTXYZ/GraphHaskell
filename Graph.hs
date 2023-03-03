72605b14a31ec0845f0e7ce8f45b81c0a43a164f
DarkTXYZ <pawaretmontfort@gmail.com>
2023-03-03 23:39:33 +0700

    - Change Vertex, Edge, Graph implementation - implement Show instance for Vertex, Edge, Graph

--- Graph.hs
+++ Graph.hs
@@ -4,145 +4,155 @@
 
 -- Graphs data
 -- Vertex
-data Vertex a = Vertex {
-    label :: String ,
-    value :: a
+data Vertex = Vertex {
+    label :: String
 }
 
-instance Show a => Show (Vertex a) where
-    show u = "\"" ++ label u ++ "\""
+instance Show Vertex where
+    show u = label u
 
-instance Eq a => Eq (Vertex a) where
+instance Eq Vertex where
     u == v = label u == label v
 
 -- Edge
-data Edge a = Edge {
-    getU :: Vertex a ,
-    getV :: Vertex a
+data Edge = Edge {
+    getU :: Vertex ,
+    getV :: Vertex
 }
 
-instance Show a => Show (Edge a) where
-    -- show :: Show a => Vertex a -> [Char]
-    show edge = show (getU edge) ++ " -- " ++ show (getV edge)
+instance Show Edge where
+    show edge = show (getU edge) ++ " <--> " ++ show (getV edge)
 
-instance Eq a => Eq (Edge a) where
-    m == n = (getU m == getU n && getV m == getV n) || (getU m == getV n && getV m == getU n)
-
--- instance Show a => Show (AdjGraph a) where
---     show g = show 
+instance Eq Edge where
+    m == n = 
+        (getU m == getU n && getV m == getV n) || 
+        (getU m == getV n && getV m == getU n)
 
 -- Graph
--- Vertex and edge list representation
-type Graph a = ([Vertex a] , [Edge a])
--- Vertex and it's adjacent vertex
-type AdjGraph a = [(Vertex a ,[Vertex a])]
+-- Vertex and Edge List representation
+data Graph = Graph {
+    vertexList :: [Vertex] ,
+    edgeList :: [Edge]
+}
+
+instance Show Graph where 
+    show graph =    "Vertices : " ++ show (vertexList graph) ++ "\n" ++
+                    "Edges : \n" ++
+                    showNewLine (edgeList graph)
+        where
+            showNewLine ls = concat $ (map ((++ "\n").show) ls)
+
+-- Adjacency List representation
+type AdjGraph = [(Vertex ,[Vertex])]
 
 -- Graph Construction
+containsVertex :: Graph -> Vertex -> Bool
+containsVertex graph u = u `elem` vertexList graph
 
-containsVertex :: Eq a => Graph a -> Vertex a -> Bool
-containsVertex graph u = u `elem` gv graph
+containsEdge:: Graph -> Edge -> Bool
+containsEdge graph u = u `elem` edgeList graph
 
-containsEdge:: Eq a => Graph a -> Edge a -> Bool
-containsEdge graph u = u `elem` ge graph
-
-addVertex :: Eq a => Graph a -> Vertex a -> Graph a
+addVertex :: Graph -> Vertex -> Graph
 addVertex graph u = 
     if containsVertex graph u 
         then
             graph
         else
-            (u : fst graph , snd graph)
+            Graph (u : vertexList graph) (edgeList graph)
 
-addVertices :: Eq a => Graph a -> [Vertex a] -> Graph a
+addVertices :: Graph -> [Vertex] -> Graph
 addVertices = foldl addVertex
 
-addEdge :: Graph a -> Edge a -> Graph a
-addEdge graph edge = (fst graph , edge : snd graph)
+addEdge :: Graph -> Edge -> Graph
+addEdge graph edge = 
+    if containsEdge graph edge
+        then
+            graph
+        else
+            Graph (vertexList graph) (edge : edgeList graph)
 
-addEdges :: Graph a -> [Edge a] -> Graph a
+addEdges :: Graph -> [Edge] -> Graph
 addEdges = foldl addEdge
 
-removeVertex ::Eq a => Graph a -> Vertex a -> Graph a 
-removeVertex graph v = (filter (\x -> x /= v) (gv graph) ,(ge (removeEdgebyVertex graph v) ))
+removeEdgebyVertex :: Graph -> Vertex -> [Edge]
+removeEdgebyVertex graph v = (filter notRemovedEdge) (edgeList graph)
+    where notRemovedEdge = \edge -> not (getU edge == v || getV edge == v)
 
-removeEdge::Eq a => Graph a -> Edge a -> Graph a
-removeEdge graph edge = fmap (filter (\x->x  /= edge)) graph
+removeVertex :: Graph -> Vertex -> Graph 
+removeVertex graph v = Graph (filter notRemovedVertex (vertexList graph)) (removeEdgebyVertex graph v)
+    where 
+        notRemovedVertex = \x -> x /= v
 
-removeEdgebyVertex :: Eq a => Graph a -> Vertex a -> Graph a
-removeEdgebyVertex graph v = fmap (filter (\edge -> not (getU edge == v|| getV edge == v))) graph
+removeEdge:: Graph -> Edge -> Graph
+removeEdge graph edge = Graph (vertexList graph) ((filter notRemovedEdge) (edgeList graph))
+    where 
+        notRemovedEdge = \x->x  /= edge
 
-allAdj ::Eq a => [Edge a] -> Vertex a -> [Vertex a]
-allAdj l v  = aux l []
-    where
-        aux [] res = res
-        aux (l:ls) res 
-         |(getU l) == v = aux ls ((getV l):res)
-         |(getV l) == v = aux ls ((getU l):res)
-         |otherwise = aux ls res
+-- allAdj ::Eq a => [Edge a] -> Vertex a -> [Vertex a]
+-- allAdj l v  = aux l []
+--     where
+--         aux [] res = res
+--         aux (l:ls) res 
+--          |(getU l) == v = aux ls ((getV l):res)
+--          |(getV l) == v = aux ls ((getU l):res)
+--          |otherwise = aux ls res
         
 
---adjacency Representation
-adjRep ::Eq a => Graph a -> AdjGraph a
-adjRep graph  = [(u,allAdj (ge graph) u)| u <- (gv graph)]
--- adjRep graph = 
---     [(u,[v]) | u <- (gv graph), 
---     v <- foldl (\acc edge -> if (getU edge == u|| getV edge == u) 
---         then if (getU edge) == u 
---                 then (getV edge):acc 
---                 else (getU edge):acc 
---               else acc ) (ge graph) ] 
-
-
-gv :: Graph a -> [Vertex a]
-gv = fst
-
-ge :: Graph a -> [Edge a]
-ge = snd
-
-
-dfs :: Graph a -> Vertex a-> [a]
-dfs graph v = 
-
--- Graph Properties
-    -- showAdjacentVertices(Vertex a)
-    -- isConnected(Vertex a , Vertex b)
-
--- Graph => [(Vertex , [Vertex])]
-
-v1 = Vertex "a" 3
-v2 = Vertex "b" 69
-v3 = Vertex "c" 6969
-v4 = Vertex "d" (-69)
-v5 = Vertex "e" 2
-v6 = Vertex "pp" 002
-v7 = Vertex "f" 1
-v8 = Vertex "g" 99
-v9 = Vertex "1xbet" 77769420
-v10 = Vertex "ggwp" 1150
-g :: Graph Int
-g = addVertices ([],[]) [v1,v2,v3,v4,v5,v6,v7,v8,v9,v10]
-gs = addEdge g (Edge v1 v2)
-gss = addEdge gs (Edge v1 v3) 
+-- --adjacency Representation
+-- adjRep ::Eq a => Graph a -> AdjGraph a
+-- adjRep graph  = [(u,allAdj (edgeList graph) u)| u <- (vertexList graph)]
+-- -- adjRep graph = 
+-- --     [(u,[v]) | u <- (vertexList graph), 
+-- --     v <- foldl (\acc edge -> if (getU edge == u|| getV edge == u) 
+-- --         then if (getU edge) == u 
+-- --                 then (getV edge):acc 
+-- --                 else (getU edge):acc 
+-- --               else acc ) (edgeList graph) ] 
 
 
 
--- gsss :: Graph Int
--- gsss = addEdge gss (Edge v1 v2)
 
--- Graph
-    -- Undirected Graph
-        -- Graph Construction
-        -- Display Graph
-        -- Get Vertex , Edges
-        -- Contains Vertex , Edges
+-- -- dfs :: Graph a -> Vertex a-> [a]
+-- -- dfs graph v = 
 
--- To Do List
-    -- Transform graph to adj list !!
-    -- Algorithm in unweighted graph    
-        -- DFS , CC
-    -- Continuation (Weighted , unweighted) ??
-    -- Algorithm 
-        -- MST
-        -- ShortestPath
-        -- Topological Sort
+v1 = Vertex "a" 
+v2 = Vertex "b" 
+v3 = Vertex "c" 
+v4 = Vertex "d" 
+v5 = Vertex "e"
 
+e12 = Edge v1 v2
+e13 = Edge v1 v3
+e21 = Edge v2 v1
+e34 = Edge v3 v4
+e45 = Edge v4 v5
+
+g :: Graph
+g = addVertices (Graph [] []) [v1,v2,v3,v4,v5]
+g1 = addEdges g [e12,e13,e21,e34,e45] 
+
+
+-- -- gsss :: Graph Int
+-- -- gsss = addEdge gss (Edge v1 v2)
+
+-- -- Graph
+--     -- Undirected Graph
+--         -- Graph Construction
+--         -- Display Graph
+--         -- Get Vertex , Edges
+--         -- Contains Vertex , Edges
+
+-- -- To Do List
+--     -- Transform graph to adj list !!
+--     -- Algorithm in unweighted graph    
+--         -- DFS , CC
+--     -- Continuation (Weighted , unweighted) ??
+--     -- Algorithm 
+--         -- MST
+--         -- ShortestPath
+--         -- Topological Sort
+
+
+
+-- -- instance Show a => Show (AdjGraph a) where
+-- --     show g = show 
