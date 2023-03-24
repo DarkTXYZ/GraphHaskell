a46805d4629a2574a59ce4e0155271ace259d8f7
DarkTXYZ <pawaretmontfort@gmail.com>
2023-03-24 19:50:04 +0700

    Update Graph.hs

--- Graph.hs
+++ Graph.hs
@@ -39,3 +39,40 @@ displayGraph graph f =
       where
         showNewLine f = concatMap (("\t" ++) . (++ "\n") . show . f)
         showAdjList l = "\t" ++ show (fst l) ++ ": " ++ show (snd l) ++ "\n"
+
+containsVertex :: Vertex -> Graph -> Bool
+containsVertex u graph = u `elem` vertexList graph
+
+addVertex :: Vertex -> Graph
+addVertex newVertex graph =
+    | containsVertex newVertex = Graph (newVertex : vertexList graph) (edgeList graph) (adjList graph)
+    | otherwise = graph
+
+addVertices :: [Vertex] -> State UndirectedGraph ()
+addVertices [] = State $ \graph -> ((), graph)
+addVertices (v : vs) = State $ \graph -> runState manip graph
+  where
+    manip = do
+        addVertex v
+        addVertices vs
+
+addVerticesFold :: [Vertex] -> State UndirectedGraph ()
+addVerticesFold vs = State $
+    \graph -> foldl (\g v -> runState (addVertex v) (snd g)) ((), graph) vs
+
+addVerticesFoldM :: [Vertex] -> State UndirectedGraph ()
+addVerticesFoldM = foldM (\_ v -> addVertex v) ()
+
+removeVertex :: Vertex -> State UndirectedGraph ()
+removeVertex vertex = State $
+    \(UDG graph) ->
+        ( ()
+        , UDG $
+            Graph
+                (filter (/= vertex) (vertexList graph))
+                ( filter
+                    (\edge -> not (getU edge == vertex || getV edge == vertex))
+                    (edgeList graph)
+                )
+                []
+        )
